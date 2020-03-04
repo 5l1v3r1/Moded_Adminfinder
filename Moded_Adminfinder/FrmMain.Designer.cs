15d0a7d69d525bcae1448341392c2151966cfeca
Mr Moded <59224173+MrModed@users.noreply.github.com>
2020-03-05 00:18:12 +0300

    Add files via upload

---
+++ Moded_Adminfinder/FrmMain.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Moded_Adminfinder
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_url = new System.Windows.Forms.TextBox();
+            this.btn_run = new System.Windows.Forms.Button();
+            this.txt_logs = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt_url
+            // 
+            this.txt_url.Location = new System.Drawing.Point(12, 12);
+            this.txt_url.Name = "txt_url";
+            this.txt_url.Size = new System.Drawing.Size(457, 20);
+            this.txt_url.TabIndex = 0;
+            // 
+            // btn_run
+            // 
+            this.btn_run.Location = new System.Drawing.Point(476, 10);
+            this.btn_run.Name = "btn_run";
+            this.btn_run.Size = new System.Drawing.Size(75, 23);
+            this.btn_run.TabIndex = 1;
+            this.btn_run.Text = "Execute";
+            this.btn_run.UseVisualStyleBackColor = true;
+            this.btn_run.Click += new System.EventHandler(this.Btn_run_Click);
+            // 
+            // txt_logs
+            // 
+            this.txt_logs.Location = new System.Drawing.Point(12, 39);
+            this.txt_logs.Multiline = true;
+            this.txt_logs.Name = "txt_logs";
+            this.txt_logs.Size = new System.Drawing.Size(539, 158);
+            this.txt_logs.TabIndex = 2;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 221);
+            this.Controls.Add(this.txt_logs);
+            this.Controls.Add(this.btn_run);
+            this.Controls.Add(this.txt_url);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Admin finder | V1.0.0 -> SuperNaturalCoding";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_url;
+        private System.Windows.Forms.Button btn_run;
+        private System.Windows.Forms.TextBox txt_logs;
+    }
+}
+
---
+++ Moded_Adminfinder/FrmMain.cs
@@ -0,0 +1,59 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Moded_Adminfinder
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_run_Click(object sender, EventArgs e)
+        {
+            List<String> AdminPathes = new List<string>();
+            AdminPathes.AddRange(File.ReadAllLines("admins.txt"));
+
+            btn_run.Enabled = false;
+            Thread t = new Thread(() =>
+            {
+                foreach (var ap in AdminPathes)
+                    Task.Factory.StartNew(() =>
+                    {
+                        Boolean isAdmin = FindAdmins(txt_url.Text, ap);
+                        if (isAdmin)
+                            txt_logs.Invoke((MethodInvoker)delegate { txt_logs.AppendText("Founded " + "http://" + txt_url.Text + "/" + "ap" + txt_url.Text); });
+                    });
+                btn_run.Invoke((MethodInvoker)delegate { this.btn_run.Enabled = true; });
+            });
+            t.Start();
+        }
+
+        private Boolean FindAdmins(String url, String adminPath)
+        {
+            try
+            {
+                var clinet = new RestClient("http://www." + url + "/" + adminPath);
+                var req = new RestRequest(Method.GET);
+
+                var resp = clinet.Get(req);
+                if (resp.StatusCode == System.Net.HttpStatusCode.OK)
+                    return true;
+
+                return false;
+            }
+            catch { return false; }
+        }
+    }
+}
